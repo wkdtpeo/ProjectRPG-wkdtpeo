8dde501003dca65a4488b8297e5720ccbecdcb5e
wkdtpeo <wkdtpeo@naver.com>
2021-06-21 22:28:38 +0900

    Initial commit

---
+++ Source/ProjectRPG/ProjectRPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectRPG, "ProjectRPG" );
---
+++ Source/ProjectRPG/ProjectRPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRPG_API AProjectRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
