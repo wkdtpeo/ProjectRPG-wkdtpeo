5b292863d842c4f58b2e9f2b6e8dbb9e8f2a2008
wkdtpeo <wkdtpeo@naver.com>
2021-06-28 00:35:22 +0900

    테이블 변경중

--- Source/ProjectRPG/GameMode/PR_GameMode_BattleStage.h
+++ Source/ProjectRPG/GameMode/PR_GameMode_BattleStage.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "../ProjectRPG.h"
+#include "../Tables/PR_TableStructs.h"
+#include <Engine/DataTable.h>
 #include "GameFramework/GameModeBase.h"
 #include "PR_GameMode_BattleStage.generated.h"
 
@@ -13,5 +15,8 @@ UCLASS()
 class PROJECTRPG_API APR_GameMode_BattleStage : public AGameModeBase
 {
 	GENERATED_BODY()
+	
+public :
+
 	
 };
