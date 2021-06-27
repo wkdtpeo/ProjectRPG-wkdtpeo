b279e0a08c505ba78ea80aadfab2fa7d262ef7d5
wkdtpeo <wkdtpeo@naver.com>
2021-06-27 16:15:50 +0900

    GameInstance, GameMode 추가

---
+++ Source/ProjectRPG/GameInstance/PR_MyGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../ProjectRPG.h"
+#include "Engine/GameInstance.h"
+#include "PR_MyGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRPG_API UPR_MyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectRPG/GameMode/PR_GameMode_BattleStage.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../ProjectRPG.h"
+#include "GameFramework/GameModeBase.h"
+#include "PR_GameMode_BattleStage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRPG_API APR_GameMode_BattleStage : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
